4ce1f326a2cd1fe08aadd9fd2a997c05ed0a2b9b
Rasmus <rasser1357@gmail.com>
2026-01-21 23:20:09 +0100

    Object oriented rewrite

--- config_loader.py
+++ config_loader.py
@@ -1,10 +1,12 @@
 import yaml
 
+
 def load_config(camera):
     with open("config.yaml", 'r') as file:
         config = yaml.safe_load(file)
 
     image_settings = config['image_settings']
+    video_settings = config['video_settings']
     lighting_settings = config['lighting_settings']
     auto_settings = config['auto_settings']
 
@@ -19,6 +21,15 @@ def load_config(camera):
     camera.Gain.Value = lighting_settings['gain']
 
 
+    if video_settings['enable_acquisition'] == "on":
+        camera.AcquisitionFrameRateEnable.Value = True
+        camera.AcquisitionFrameRate.Value = video_settings['acquisition_fps']
+    elif video_settings['enable_acquisition'] == "off":
+        camera.AcquisitionFrameRateEnable.Value = False
+    else:
+        raise ValueError("Invalid enable_acquisition value in config.yaml")
+
+
     camera.AutoTargetBrightness.Value = auto_settings['auto_brightness_target']
 
 
@@ -28,6 +39,8 @@ def load_config(camera):
         camera.ExposureAuto.Value = "Once"
     elif auto_settings['auto_exposure'] == "continuous":
         camera.ExposureAuto.Value = "Continuous"
+    else:
+        raise ValueError("Invalid auto_exposure value in config.yaml")
 
     camera.AutoExposureTimeLowerLimit.Value = auto_settings['auto_exposure_lower_limit']
     camera.AutoExposureTimeUpperLimit.Value = auto_settings['auto_exposure_upper_limit']
@@ -37,6 +50,8 @@ def load_config(camera):
         camera.AutoFunctionProfile.value = "MinimizeGain"
     elif auto_settings['auto_function'] == "min_exposure":
         camera.AutoFunctionProfile.value = "MinimizeExposureTime"
+    else:
+        raise ValueError("Invalid auto_function value in config.yaml")
 
 
     if auto_settings['auto_gain'] == "off":
@@ -45,6 +60,8 @@ def load_config(camera):
         camera.GainAuto.Value = "Once"
     elif auto_settings['auto_gain'] == "continuous":
         camera.GainAuto.Value = "Continuous"
+    else:
+        raise ValueError("Invalid auto_gain value in config.yaml")
 
     camera.AutoGainLowerLimit.Value = auto_settings['auto_gain_lower_limit']
     camera.AutoGainUpperLimit.Value = auto_settings['auto_gain_upper_limit']
--- main.py
+++ main.py
@@ -4,66 +4,82 @@ import cv2
 from config_loader import load_config
 
 
-camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
-camera.Open()    
+class camera_control:
+    def __init__(self):
+        self.camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
+        self.camera.Open()
+        load_config(self.camera)
+        self.check_for_input()
 
 
-def snap_pics(numberOfImagesToGrab):
-    camera.StartGrabbingMax(numberOfImagesToGrab)
+    def snap_pic(self):
+        self.camera.StartGrabbingMax(1)
 
-    while camera.IsGrabbing():
-        grabResult = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+        while self.camera.IsGrabbing():
+            grabResult = self.camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+            if grabResult.GrabSucceeded():
+                # Access the image data.
+                img = grabResult.Array
+                print("Gray value of first pixel: ", img[0, 0])
 
-        if grabResult.GrabSucceeded():
-            # Access the image data.
-            print("SizeX: ", grabResult.Width)
-            print("SizeY: ", grabResult.Height)
-            img = grabResult.Array
-            print("Gray value of first pixel: ", img[0, 0])
-
-        grabResult.Release()
-    camera.Close()
+            grabResult.Release()
+        self.camera.Close()
 
 
-def stream():
-    camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly) 
+    def stream(self):
+        self.camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly) 
 
-    # Converter til OpenCV format (fra Basler format til BGR/RGB)
-    converter = pylon.ImageFormatConverter()
-    converter.OutputPixelFormat = pylon.PixelType_BGR8packed
-    converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
+        # Converter til OpenCV format (fra Basler format til BGR/RGB)
+        converter = pylon.ImageFormatConverter()
+        converter.OutputPixelFormat = pylon.PixelType_BGR8packed
+        converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
 
-    print("Live view kører... Tryk på 'q' for at afslutte.")
+        print("Live view kører... Tryk på 'q' for at afslutte.")
 
-    while camera.IsGrabbing():
-        grabResult = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+        while self.camera.IsGrabbing():
+            grabResult = self.camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
 
-        if grabResult.GrabSucceeded():
-            # Konverter billedet til et format OpenCV kan forstå (numpy array)
-            image = converter.Convert(grabResult)
-            frame = image.GetArray()
+            if grabResult.GrabSucceeded():
+                # Konverter billedet til et format OpenCV kan forstå (numpy array)
+                image = converter.Convert(grabResult)
+                frame = image.GetArray()
 
-            # Vis billedet i et vindue
-            cv2.imshow('Basler ace 2 Live View', frame)
+                # Vis billedet i et vindue
+                cv2.imshow('Basler ace 2 Live View', frame)
 
-            # Stop hvis brugeren trykker på 'q'
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+                # Stop hvis brugeren trykker på 'q'
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
 
-        grabResult.Release()
+            grabResult.Release()
 
-    # Ryd op
-    camera.StopGrabbing()
-    camera.Close()
-    cv2.destroyAllWindows()
+        # Ryd op
+        self.camera.StopGrabbing()
+
+        cv2.destroyAllWindows()
+
+    
+    def check_for_input(self):
+        try:
+            while True:
+                user_input = input("Enter 'p' to take a picture, 's' to start live view, or 'q' to quit: ")
+                if user_input == 'p':
+                    self.snap_pic()
+                elif user_input == 's':
+                    self.stream()
+                elif user_input == 'q':
+                    break
+                else:
+                    print("Invalid input. Please try again.")
+
+        finally:
+            print("Lukker kameraet")
+            if hasattr(self, 'camera') and self.camera.IsOpen():
+                self.camera.Close()
 
 
 
 if __name__ == "__main__":
-    load_config(camera)
-
-    #snap_pics(1)
-
-    stream()
+    camera_control()
 
 
